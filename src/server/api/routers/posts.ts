c95414a4cc45c03342779a5036c72de38d0b54a1
fawadniazi <fawad.niazi@gmail.com>
2023-05-12 18:10:29 +0800

    Next image update, style update and data display changes added postview with type inference from Router output

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,48 @@
+import { auth, clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/api";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    //.map -> filterUserForClient
+
+    console.table(users);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId); // Very important
+
+      if (!author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for this post is not found",
+        });
+
+      return {
+        post,
+        author,
+      };
+    });
   }),
 });
